5a593c3d845dedb3c9d97b4dc9aa02230e13f263
THAWIWAT05 <thawiwat05254905@gmail.com>
2025-10-31 23:58:47 +0700

    Add files via upload

---
+++ src/assets/mock/mock.js
@@ -0,0 +1,57 @@
+import floating1 from "../images/floating/3.gif";
+import floating2 from "../images/floating/4.gif";
+import image1 from "../images/1.png";
+import image2 from "../images/2.png";
+import image3 from "../images/3.png";
+import image4 from "../images/4.png";
+import image5 from "../images/5.png";
+import image6 from "../images/6.png";
+import image7 from "../images/7.png";
+import gift1 from "../images/gifts/1.gif";
+import gift2 from "../images/gifts/3.gif";
+import gift3 from "../images/gifts/4.gif";
+import gift4 from "../images/gifts/2.gif";
+import gift5 from "../images/gifts/5.gif";
+import gift6 from "../images/gifts/2.png";
+import gift7 from "../images/gifts/5.png";
+import star from "../star.svg";
+
+export const _floatingImageFirst = floating1;
+export const _floatingImageSecond = floating2;
+
+export const _albums = [image1, image2, image3, image4, image5, image6, image7];
+
+export const giftAlbums = [gift1, gift2, gift3, gift4, gift5];
+
+export const _stars = [
+    {
+        src: star,
+        color: "#ffccd7",
+        size: "12",
+    },
+    {
+        src: star,
+        color: "#ffccd7",
+        size: "4",
+    },
+];
+
+export const _messages = [
+    "วันนี้เค้าตั้งใจจะทำอะไรที่พิเศษใส่ไข่ให้เทอ มันอาจจะไม่ได้อะไรมาก อาจจะกิ๊กก็อกๆๆ 555 ",
+    "หวังว่าเธอจะมีความสุขในวันเกิดนี้มากๆเว็บนี้จะเป็นส่งที่มีค่าที่สุดที่ให้ได้ละ แฮร่ๆๆๆๆๆ",
+];
+
+export const _birthdayMessages = [
+    "สุขสันต์วันเกิดล้วงหน้านะคะ!",
+    "วันนี้ถือว่าเป็นการอวยพรวันเกิดล้วงหน้า",
+    "ก็ไม่รู้จะอวยพรยังไง แต่ก็ ขอให้เทอมีความสุขมากๆๆนะ ขอให้เป็นคุณพยาบาลสมใจอยาก 5555",
+    "ก็เลิกอ่านนิยายได้ละ เดียวขอบตาดำเป็นหมีแพ้นด้า",
+    "ไม่รู้จะพูดไร แต่สิ่งนี้คงเป็นของขวัญชิ้นสุดท้าย ที่ได้ให้ กลับมาดูได้ตลอดเลยน้าาา ถ้ามันไม่ปิด 5555 Always love and care @Tiw",
+    "สุขสันต์วันเกิดน้าาาาาา",
+];
+
+// เอาแค่ code ของคลิปมาใส่ หลัง https://www.youtube.com/embed/ เช่น https://youtube.com/shorts/equ0y9yuLYk จะใช้แค่ equ0y9yuLYk
+// https://www.youtube.com/embed/equ0y9yuLYk
+export const _youtubeUrl = "https://www.youtube.com/embed/gDTDFz0PL78?si=_NWbXqFKt1f8e7p6";
+
+export const _otp = "fai";
